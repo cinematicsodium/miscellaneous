d4e47c5bdbe10006937f501d0324c22d4a4290c3
Name <no@email.com>
2024-02-16 20:14:44 -0700

    Save changes.

--- HackerRank_phonebookQuery.py
+++ HackerRank_phonebookQuery.py
@@ -1,11 +1,3 @@
-'''
-# 1. Create a phone book using a dictionary data structure.
-# 2. Read the number of entries in the phone book.
-# 3. Read the name and phone number for each entry and add it to the phone book.
-# 4. Read the queries and print the corresponding phone number for each query.
-# 5. If the name is not found in the phone book, print "Not found".
-'''
-
 phonebookDict: dict = {}
 
 numEntries: int = int(input())
