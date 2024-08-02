5ad51858d06f2373b3e0b61334a505af7ffe84a1
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2024-08-02 16:04:15 -0600

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,115 +1,78 @@
-import secrets as s
-import sys
+from secrets import choice
 
-def random_password():
-    # User Input
-    # Length of the password
-    attempt: int = 0
-    while attempt < 5:
-        try:
-            passLength: int = int(input("\nEnter the length of the password (range: 6-32 characters): "))
-            if passLength not in range(6, 33):
-                attempt += 1
-                if attempt == 5:
-                    print("\nMaximum number of attempts reached. Exiting.")
-                    sys.exit()
-                print("\nInvalid input. Please enter a number between 6 and 32.")
-                continue
-            break
-        except ValueError:
-            attempt += 1
-            if attempt == 5:
-                print("\nMaximum number of attempts reached. Exiting.")
-                sys.exit()
-            print("\nInvalid input. Please enter a numeric value.")
+NOUNS_PATH = r'username_password_generator/nounsList.txt'
 
-    validInput: list = ['n', 'no', 'y', 'yes']
-    
-    # Check if user wants a special character in the password
-    charCheck: bool = False
-    attempt: int = 0
-    while attempt < 3:
-        spChar: str = input("\nDo you want a special character in your password? (Y/N): ").lower()
-        if spChar in validInput:
-            if spChar in validInput[2:]:
-                charCheck = True
-                passLength -= 1
-                break
-            break
-        else:
-            attempt += 1
-            if attempt == 3:
-                print("\nMaximum number of attempts reached. Exiting.")
-                sys.exit()
-            print("\nInvalid input. Please enter 'Y' or 'N'.")
 
-    # Check if user wants a number in the password
-    numCheck: bool = False
-    attempt = 0
-    while attempt < 3:
-        containsNumbers: str = input("\nDo you want a number in your password? (Y/N): ").lower()
-        if containsNumbers in validInput:
-            if containsNumbers in validInput[2:]:
-                numCheck = True
-                passLength -= 1
-                break
-            break
-        else:
-            attempt += 1
-            if attempt == 3:
-                print("\nMaximum number of attempts reached. Exiting.")
-                sys.exit()
-            print("\nInvalid input. Please enter 'Y' or 'N'.")
-  
-    # Check if user wants to generate multiple passwords
-    attempt: int = 0
-    while attempt < 3:
-        try:
-            passCount: int = int(input("\nHow many passwords do you want to generate? (range: 1-32 passwords): "))
-            if passCount not in range(1, 33):
-                attempt += 1
-                if attempt == 3:
-                    print("\nMaximum number of attempts reached. Exiting.")
-                    sys.exit()
-                print("\nInvalid input. Please enter a number between 1 and 32.")
-                continue
-            break
-        except ValueError:
-            attempt += 1
-            if attempt == 3:
-                print("\nMaximum number of attempts reached. Exiting.")
-                sys.exit()
-            print("\nInvalid input. Please enter a numeric value.")
+def get_noun() -> str:
+	nouns: list = []
+	noun: str = ''
+	coin: int = choice(range(2))
+	with open(NOUNS_PATH, 'r') as f:
+		nouns = f.read().splitlines()
+		noun: str = str(choice(nouns))
+	if coin == 1:
+		noun = noun.capitalize()
+	return noun
 
-  # Password Constraints
-    consonants: list = ['b', 'd', 'f', 'g', 'j', 'k', 'm', 'n', 'p', 'r', 's', 't', 'z']
-    vowels: list = ['a', 'e', 'i', 'o', 'u']
-    specials: list = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '?', '=', '~']
-    numbers: list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']    
-    
-    # Generate Password
-    pw = 1
-    for i in range(passCount):
-        randChar: int = s.randbelow(passLength + 1)
-        randNum: int = s.randbelow(passLength + 1)
-        password: list = []
-        max_consonant_vowel_pairs: int = passLength // 2
+def nouns_password() -> None:
+	print()
+	print('Nouns Password')
+	print()
+	for i in range(3):
+		nounsList: list[str] = [
+		get_noun(),
+		get_noun(),
+		get_noun(),
+		]
+		password: str = '-'.join(nounsList) + "-" + str(choice(range(100))).zfill(2) +' '
+		print(f"\tpassword: {password.ljust(40,'.')} len: {len(password)}")
+		print()
 
-        for i in range(1, max_consonant_vowel_pairs + 1):
-            password.append(s.choice(consonants))
-            password.append(s.choice(vowels))
+def consonant_vowel_password(passLen: int = 0) -> None:
+	consonants: list[str] = [
+		'b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'x', 'y', 'z'
+	]
+	vowels: list[str] = [
+		'a', 'e', 'i', 'o', 'u'
+	]
+	specials: list[str] = [
+		'!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '?', '=', '~'
+	]
+	numbers: list[str] = [
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+	]
+	
+	print('\nConsonant-Vowel Passwords\n')
+	
+	if not passLen:
+		while True:
+			try:
+				passLen = int(input('Enter a password length: '))
+				if passLen <= 0:
+					raise ValueError
+				break
+			except ValueError:
+				print("Please enter a valid positive integer.")
+	
+	for _ in range(3):
+		password: list[str] = [choice(specials)]
+		for i in range(passLen):
+			if i % 2 == 0:
+				password.append(choice(consonants))
+			else:
+				password.append(choice(vowels))
+			if (i + 1) % 4 == 0 and i != passLen - 1:
+				password.append("-")
+		
+		password.append("-")
+		password.extend(choice(numbers))
+		password.append(' ')
+		
+		final_password = ''.join(password)
+		print(f"\tpassword: {final_password.ljust(40,'.')} len: {len(final_password)}\n")
+	
+if __name__ == '__main__':
 
-        if charCheck:
-            password.insert(randChar, s.choice(specials))
-        if numCheck: 
-            password.insert(randNum, s.choice(numbers))
-        
-        passString = ' '.join([''.join(password[i:i+4]) for i in range(0, len(password), 4)])
-        # passString = ''.join(str(i) for i in password)
+	consonant_vowel_password(passLen=16)
+	nouns_password()
 
-        print(f'Pw_{pw}: \t{passString}')
-        pw += 1
-
-    print('\nSpaces are added for readability.\n')
-
-random_password()
